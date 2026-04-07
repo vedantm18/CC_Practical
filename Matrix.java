4ea480bf3721e81c5aa88d6ba1049aa1edbfa937
Vedant Mote <vedantmote79@gmail.com>
2026-04-07 14:42:36 +0530

    Create Matrix.java

---
+++ Matrix.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class AddMatrix{
+
+   static void addMatrix(int arr1[][],int arr2[][]){
+      int arr3[][];
+
+     for(int i=0;i<arr[0].length;i++){
+       for(int j=0;j<arr1.length;j++){
+       arr3[i][j]=arr1[i][j]+arr2[i][j];
+}
+  
+}
+ for(int i=0;i<arr3[0].length;i++){
+   for(int j=0;i<arr.length;j++){
+     System.out.println(arr[i][j];
+   }
+}
+
+    
+   }
+public static void main(String args[]){
+ int arr1[][]={{1,2,3},{4,5,6},{7,8,9}};
+ int arr2[][]={{1,2,3},{4,5,6},{7,8,9}};
+  addMatrix(arr1,arr2);
+}
+}
