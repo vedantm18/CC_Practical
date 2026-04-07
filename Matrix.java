ec75505daa764ccb5b57eac0afa5a593204ca506
Vedant Mote <vedantmote79@gmail.com>
2026-04-07 14:54:34 +0530

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 public class AddMatrix{
 
    static void addMatrix(int arr1[][],int arr2[][]){
-      int arr3[][];
+      int arr3[][]=new int[3][3];
 
-     for(int i=0;i<arr[0].length;i++){
-       for(int j=0;j<arr1.length;j++){
+     for(int i=0;i<arr1.length;i++){
+       for(int j=0;j<arr1[0].length;j++){
        arr3[i][j]=arr1[i][j]+arr2[i][j];
 }
   
 }
- for(int i=0;i<arr3[0].length;i++){
-   for(int j=0;i<arr.length;j++){
+ for(int i=0;i<arr3.length;i++){
+   for(int j=0;i<arr3[0].length;j++){
      System.out.println(arr[i][j]);
    }
 }
